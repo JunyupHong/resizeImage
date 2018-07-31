0caebaaa2bf938a46d8505ba0c8fd8bea79477b8
Junyup Hong <junyup.hong@gmail.com>
2018-07-31 18:13:21 +0900

    resizeByPercent 수정

--- resizingImage.js
+++ resizingImage.js
@@ -28,24 +28,13 @@ exports.resize = (width, height, from, to, form, size) => {
 };
 
 
-exports.resizeByPercent = (percent, imageWidth, imageHeight, from, to, form) => {
-
-    const executeResize = () => {
-        for (let i = 0; i < rawFiles.length; i++) {
-            const fileName = rawFiles[i].split('.')[0];
-
-            sharp(`./${from}/${rawFiles[i]}`)
-                .rotate()
-                .resize(imageWidth * percent, imageHeight * percent)
-                .toFile(`./${to}/${fileName}_${size}.${form}`);
-        }
-    };
-
-    const rawFiles = fs.readdirSync(`${from}`);
-
+exports.resizeByPercent = (percent, imageWidth, imageHeight, fileName, from, to, form) => {
     fs.mkdir(`./${to}`, function (err) {
         if (err) ;
-        executeResize();
+        sharp(`./${from}/${fileName}`)
+            .rotate()
+            .resize(imageWidth * percent, imageHeight * percent)
+            .toFile(`./${to}/${fileName}_midium.${form}`);
     });
 };
 
